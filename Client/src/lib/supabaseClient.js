e429a4ae0742cb5e87106628fcdece700d3bed22
zeeshanhussainn <zeeshanhussain534@gmail.com>
2025-10-13 16:45:16 +0530

    changed dist location

--- Client/src/lib/supabaseClient.js
+++ Client/src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY;
 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
