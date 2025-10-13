4c6a9511917b8f01c3d9ed47ab836a60f63752cc
zeeshanhussainn <zeeshanhussain534@gmail.com>
2025-10-13 15:47:18 +0530

    Updated server.js only

--- server.js
+++ server.js
@@ -83,7 +83,11 @@ const __dirname = path.resolve();
 const buildPath = path.join(__dirname, "dist"); // Vite output
 app.use(express.static(buildPath));
 
-app.get("/*", (req, res) => {
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(buildPath, "index.html"));
+// });
+
+app.use((req, res, next) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
