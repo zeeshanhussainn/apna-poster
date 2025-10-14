f855768ad701da10ea009ef29bc4da9302797c4e
JyotikaAmbalge <jyotikaambalge.com>
2025-10-14 12:44:01 +0530

    Testing GIT

--- server.js
+++ server.js
@@ -94,3 +94,6 @@ app.use((req, res, next) => {
 // app.listen(5000, () => console.log("Server running on port 5000"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+//hello buddy 
